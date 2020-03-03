67c84b28346e27d9dc4cc0cbaa064567203c2459
francescodt <francescodtroiano@gmail.com>
2020-03-02 21:01:12 -0600

    some js work, much to do

--- js/app.js
+++ js/app.js
@@ -10,11 +10,10 @@ var storeOne = {
     minCust: 23,
     maxCust: 65,
     averageSale: 6.3,
-    custNumHour: 0,
+    hourArray: [],
     setCustHour: function() {
         this.custNum = randomCustNum(23, 65);
     }
-
 
 }
 
@@ -25,7 +24,7 @@ var storeTwo = {
     minCust: 3,
     maxCust: 24,
     averageSale: 1.2,
-    custNumHour: 0,
+    hourArray: [],
     setCustHour: function() {
         this.custNum = randomCustNum(3, 24);
     }
@@ -39,7 +38,7 @@ var storeThree = {
     minCust: 11,
     maxCust: 38,
     averageSale: 3.7,
-    custNumHour: 0,
+    hourArray: [],
     setCustHour: function() {
         this.custNum = randomCustNum(11, 38);
     }
@@ -53,7 +52,7 @@ var storeFour = {
     minCust: 20,
     maxCust: 38,
     averageSale: 2.3,
-    custNumHour: 0,
+    hourArray: [],
     setCustHour: function() {
         this.custNum = randomCustNum(20, 38);
     }
@@ -67,11 +66,10 @@ var storeFive = {
     minCust: 2,
     maxCust: 16,
     averageSale: 4.6,
-    custHour: 0,
+    hourArray: [],
     setCustHour: function() {
         this.custHour.push(randomCustHour(2, 16));
     }
-    
 
 }
 
@@ -80,7 +78,9 @@ function randomCustHour(min, max) {
 }
 
 
+
 // 14 positions 0-13
+
 // var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm','7pm'];
 
 // var arrayLength = timeOfDay.length;
@@ -92,6 +92,16 @@ function randomCustHour(min, max) {
 //     actualTime = (i + 1);
 
     
+
+
+
+
+
+
+
+
+
+
 
 //actual html things
 var parentElement = document.getElementById('storeProfile');
