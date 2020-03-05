1dcfa420997847f7aba576494271b0528b821e3b
francescodt <francescodtroiano@gmail.com>
2020-03-05 15:52:09 -0600

    css work, js forms added, form added to html

--- js/constructor.js
+++ js/constructor.js
@@ -37,16 +37,22 @@ var paris = new Stores('Paris', 20, 38, 2.3);
 var lima = new Stores('Lima', 2, 16, 4.6);
 
 
+
+//Table
 var storesTable = document.getElementById('sales');
 var tableRow = document.createElement('tr');
 var firstCell = document.createElement('td');
-firstCell.textContent = 'blubber';
+firstCell.textContent = ' ';
 tableRow.appendChild(firstCell);
 for (var i = 0; i < hours.length; i++) {
     var hoursCell = document.createElement('td');
     hoursCell.textContent = hours[i];
     tableRow.appendChild(hoursCell);
 }
+
+var lastCell = document.createElement('td');
+lastCell.textContent = 'Total of Totals';
+tableRow.appendChild(lastCell);
 
 storesTable.appendChild(tableRow);
 
@@ -72,20 +78,23 @@ Stores.prototype.render = function () {
 
 var storesLocations = [seattle, tokyo, dubai, paris, lima]
 
+//loop
 for (var i = 0; i < storesLocations.length; i++) {
     storesLocations[i].setCustomers();
     storesLocations[i].render();
 }
 
-
+//Footer
 var tableFooter = document.createElement('tr');
 
+//first row
 var totalRow = document.createElement('td');
 totalRow.textContent = 'Total';
 tableFooter.appendChild(totalRow);
 
+//math total
 var hourTotal = 0;
-for (var i = 0; i < hours.length; i++) {
+for (var i = 0; i < hours.length + 1; i++) {
     hourTotal = 0;
     for (var j = 0; j < storesLocations.length; j++) {
         hourTotal += storesLocations[j].cookies[i];
@@ -96,3 +105,98 @@ for (var i = 0; i < hours.length; i++) {
 }
 
 storesTable.appendChild(tableFooter);
+
+
+//new form stuff below //
+
+// still to do
+//call function to keep total at bottom
+
+// take in an event parameter so that we can prevent the default
+function handleFormSubmitted(event){
+    event.preventDefault();
+    console.log(event);
+
+    var name = document.getElementById('name').value
+    var minCus = document.getElementById('minCus').value
+    var maxCus = document.getElementById('maxCus').value
+    var averageSales = document.getElementById('averageSales').value
+
+
+    var stores = new Stores(name, minCus, maxCus, averageSales);
+
+    stores.setCustomers();
+    stores.render();
+
+    var form = document.getElementById('newCity');
+    form.reset();
+
+
+}
+
+var formElement = document.getElementById('newCity');
+
+formElement.addEventListener('submit', handleFormSubmitted);
+
+
+
+
+
+
+
+
+////////////////////////
+//add array to the constructor function
+
+// set up event listener to listen to the submit event
+//1. Which element do we need
+
+//2. Which event am I listening to?
+
+
+//3. What code should I run when that event happens?
+
+// // take in an event parameter so that we can prevent the default
+// function handleFormSubmitted(event){
+//     event.preventDefault();
+//     console.log(event);
+
+
+// var nameInput = doument.getElementById('firstsectionformanswer1')
+// var nameValue = nameInput['value'];
+// var breedInput = document.getElementById('secondsectionformanswers2');
+// var breedValue = breedInput['value'];
+// var otherInput = document.getElementById('thirdsectionformanswers3');
+// var otherValue = otherInput.value;
+// var interestsInput = document.getElementById('fourhtsectionformanswer4');
+// var interestsValue = interestsInput.value;
+
+
+// //add in checkbox values
+// var isGoodWithKids = event.target.isGoodWithKids.checked;
+// var isGoodwithDogs = event.target.isGoodwithDogs.checked;
+
+// var newPet = new newPet(nameValue, breedValue, otherValue, interestsValue, isGoodWithKids, isGoodwithDogs);
+
+//newPet.setAge();
+//newPet.render();
+//newpet.getInterests();
+
+// //add array to the constructor function
+// newPet.allPets.push(this);
+// newPet.allPets = [];
+
+
+// var form?????
+
+// }
+
+// // set up event listener to listen to the submit event
+// //1. Which element do we need
+// var formElement = document.getElementById('new-pets');
+
+// //2. Which event am I listening to?
+
+
+// //3. What code should I run when that event happens?
+// formElement.addEventListener('subtmit', handleFormSubmitted);
